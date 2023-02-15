523500d5489533e40b630f24725d2689ee841f1b
jesse-crypted <98085822+jesse-crypted@users.noreply.github.com>
2023-02-15 19:25:20 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,30 +19,3 @@ exports.getAllUsers = async (req, res) => {
   }
 };
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
